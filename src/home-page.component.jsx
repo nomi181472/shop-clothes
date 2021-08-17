468f4c717ac961047a264bdfd9e4aa2dcd2cc924
noman181472 <nomansoomro51@gmail.com>
2021-08-18 00:59:34 +0500

    home page added

---
+++ src/home-page.component.jsx
@@ -0,0 +1,38 @@
+import "./homepage.styles.scss"
+export const HomePage=()=>{
+return <div className="homepage">
+<div className="directory-menu">
+<div className="menu-item">
+<div className="content">
+<h1 className="title">Hats</h1>
+<span className="subtitle"> Shop Now</span>
+</div>
+</div>
+<div className="menu-item">
+<div className="content">
+<h1 className="title">Hats</h1>
+<span className="subtitle"> Shop Now</span>
+</div>
+</div>
+<div className="menu-item">
+<div className="content">
+<h1 className="title">Hats</h1>
+<span className="subtitle"> Shop Now</span>
+</div>
+</div>
+<div className="menu-item">
+<div className="content">
+<h1 className="title">Hats</h1>
+<span className="subtitle"> Shop Now</span>
+</div>
+</div>
+<div className="menu-item">
+<div className="content">
+<h1 className="title">Hats</h1>
+<span className="subtitle"> Shop Now</span>
+</div>
+</div>
+</div>
+
+</div>
+}
